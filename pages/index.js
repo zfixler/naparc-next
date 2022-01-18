3420333b40aa435283482aa25b7c7d18450c1397
zfixler <zakfixler@gmail.com>
2022-01-17 20:29:51 -0500

    added og image

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,15 @@ export default function Home() {
 		<SearchContextProvider>
 			<Head>
 				<title>NAPARC Search</title>
-				<meta name="description" content="Find a reformed congregation near you with NAPARC Search." />
+				<meta
+					name="description"
+					content="Find a reformed congregation near you with NAPARC Search."
+				/>
+				<meta property="og:title" content="NAPARC Search" />
+				<meta
+					property="og:image"
+					content="https://res.cloudinary.com/dke6nfk5z/image/upload/v1642469357/naparc-next-og_pleeaa.png"
+				/>
 			</Head>
 			<header>
 				<h1 className="headline">Find a Reformed congregation near you.</h1>
